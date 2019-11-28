83b50e2b56ad9d5e270f9793b37223a1bc2e258e
jiaqinggang1 <forestjqg@126.com>
2019-11-28 01:39:17 -0800

    added mocked contract

---
+++ test/helper/FinNexusContributionMock.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.4.24;
+
+import '../../contracts/FinNexusContribution.sol';
+
+contract FinNexusContributionMock is FinNexusContribution{
+
+    function FinNexusContributionMock {
+
+    }
+
+
+	function setMockedOpenSoldTokensRemain(uint remainToken){
+		openSoldTokens = MAX_OPEN_SOLD - remainToken * (10**18);
+	}
+
+    function setMockedMaxOpenSoldTokens(uint _maxopenSoldTokens){
+        MAX_OPEN_SOLD = _maxopenSoldTokens;
+    }
+}
